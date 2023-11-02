a185dc738077e9a54aa7c8eace75c82a7a44f751
Boumour Mouad <mouadboum2001@gmail.com>
2023-11-02 23:59:38 +0100

    Architecture des couches

--- matchmaking/src/main/java/com/sqli/matchmaking/Model/User.java
+++ matchmaking/src/main/java/com/sqli/matchmaking/Model/User.java
@@ -1,4 +1,4 @@
-package com.sqli.matchmaking.user;
+package com.sqli.matchmaking.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- matchmaking/src/main/java/com/sqli/matchmaking/Repository/UserRepository.java
+++ matchmaking/src/main/java/com/sqli/matchmaking/Repository/UserRepository.java
@@ -1,6 +1,9 @@
-package com.sqli.matchmaking.user;
+package com.sqli.matchmaking.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sqli.matchmaking.Model.User;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
